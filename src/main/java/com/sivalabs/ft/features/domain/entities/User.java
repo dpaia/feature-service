25e4c120408e9be4d929f730bfc1853cb8c06f64
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2026-03-02 18:56:26 +0100

    nes-006: add @Id annotation to existing id field
    caret: src/main/java/com/sivalabs/ft/features/domain/entities/User.java:146

--- src/main/java/com/sivalabs/ft/features/domain/entities/User.java
+++ src/main/java/com/sivalabs/ft/features/domain/entities/User.java
@@ -1,10 +1,12 @@
 package com.sivalabs.ft.features.domain.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class User {
+    @Id
     private Long id;
 }
