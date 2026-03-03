83f86570a176dc5f41c57441ec80e8863183f7ef
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2026-03-03 18:14:47 +0100

    nes-017: mapper with @Mapping on toDto, empty line above toEntity

---
+++ src/main/java/com/sivalabs/ft/features/domain/mappers/MilestoneMapper.java
@@ -0,0 +1,14 @@
+package com.sivalabs.ft.features.domain.mappers;
+
+import com.sivalabs.ft.features.domain.dtos.MilestoneDto;
+import com.sivalabs.ft.features.domain.entities.Milestone;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface MilestoneMapper {
+    @Mapping(target = "completedFeatures", source = "resolvedFeatures")
+    MilestoneDto toDto(Milestone milestone);
+
+    Milestone toEntity(MilestoneDto dto);
+}
