d24e932d229b2672af58b2cf942811b304396d12
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2026-03-03 19:41:28 +0100

    nes-023: ReleaseNote with author field not in constructor

---
+++ src/main/java/com/sivalabs/ft/features/domain/entities/ReleaseNote.java
@@ -0,0 +1,46 @@
+package com.sivalabs.ft.features.domain.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "release_notes")
+public class ReleaseNote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    private String author;
+
+    public ReleaseNote(Long id, String title, String content) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
