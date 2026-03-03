2fcf42404edf20335d8d0301b2f161b89c8b2b6d
Konstantin Aleev <konstantin.aleev@jetbrains.com>
2026-03-03 19:32:17 +0100

    nes-022: Lombok entity with @AllArgsConstructor, caret inside class body

---
+++ src/main/java/com/sivalabs/ft/features/domain/entities/Priority.java
@@ -0,0 +1,24 @@
+package com.sivalabs.ft.features.domain.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Table(name = "priorities")
+@Data
+@AllArgsConstructor
+public class Priority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int level;
+    private String description;
+
+}
