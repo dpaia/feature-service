191048f6792e0632bd9b1eb6c385dd00f4158a31
innokenty <innocentshuvalov@gmail.com>
2026-03-19 17:07:41 +0100

    tmp: break test assertion to test workflow

--- src/test/java/com/sivalabs/ft/features/api/controllers/ReleaseControllerTests.java
+++ src/test/java/com/sivalabs/ft/features/api/controllers/ReleaseControllerTests.java
@@ -76,7 +76,7 @@ class ReleaseControllerTests extends AbstractIT {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(payload)
                 .exchange();
-        assertThat(result).hasStatusOk();
+        assertThat(result).hasStatus(HttpStatus.CREATED);
 
         // Verify the update
         var updatedRelease =
