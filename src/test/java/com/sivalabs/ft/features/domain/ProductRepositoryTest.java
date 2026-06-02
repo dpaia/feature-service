6865299eabe63b33f825c614a346208df846bd25
Evgenii Zakharchenko <evgenii.zakharchenko@jetbrains.com>
2026-06-02 13:00:52 +0200

    Apply Spotless formatting to historical base

--- src/test/java/com/sivalabs/ft/features/domain/ProductRepositoryTest.java
+++ src/test/java/com/sivalabs/ft/features/domain/ProductRepositoryTest.java
@@ -1,13 +1,13 @@
 package com.sivalabs.ft.features.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.sivalabs.ft.features.TestcontainersConfiguration;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
 @Import(TestcontainersConfiguration.class)
@@ -17,9 +17,10 @@ public class ProductRepositoryTest {
     private ProductRepository productRepository;
 
     @Test
-    void testFindByCode(){
-        Product productByCode = productRepository.findByCode("intellij").orElseThrow(() -> new EntityNotFoundException("Product not found"));
+    void testFindByCode() {
+        Product productByCode = productRepository
+                .findByCode("intellij")
+                .orElseThrow(() -> new EntityNotFoundException("Product not found"));
         assertEquals("intellij", productByCode.getCode(), "Product code does not match condition");
     }
-
 }
